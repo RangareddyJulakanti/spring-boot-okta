a222132194bf761b3a25bd465905536b0129678a
ranga <rangareddyjava9@gmail.com>
2020-01-28 01:18:17 +0530

    Added endpoint

--- src/main/java/com/ranga/okta/developer/demo/DemoApplication.java
+++ src/main/java/com/ranga/okta/developer/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.ranga.okta.developer.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -12,6 +13,7 @@ import java.security.Principal;
 @EnableOAuth2Sso
 public class DemoApplication {
 
+	@GetMapping("/")
 	public String welcomeToUser(Principal principal){
 		return "Hi"+ principal.getName()+"Welcome to  Okta";
 	}
