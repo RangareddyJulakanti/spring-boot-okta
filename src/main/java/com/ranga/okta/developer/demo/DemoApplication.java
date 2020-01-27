98b88ce76ca7433adc1bd69cfc1f653da7934ae1
ranga <rangareddyjava9@gmail.com>
2020-01-28 01:08:32 +0530

    Initial commit

---
+++ src/main/java/com/ranga/okta/developer/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.ranga.okta.developer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+@EnableOAuth2Sso
+public class DemoApplication {
+
+	public String welcomeToUser(Principal principal){
+		return "Hi"+ principal.getName()+"Welcome to  Okta";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
